5768bae21fc0fab2d09b16ff6126c9d34340991f
avatarsik <avatarsik6699@gmail.com>
2020-10-05 12:18:16 +0700

    refactoring button, radio, field, toggle. Added styles and html for the registration form.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import './style.scss';
+// import { Calendar } from 'root/UI_kit/cards/components/calendar/calendar';
 
-import { Dropdown } from 'FEcomponents/dropdown/dropdown';
-new Dropdown('div[data-id="1"]',{});
-let guests = new Dropdown('div[data-id="2"]',{});
+// new Calendar('input[data-id="1"]', {
+//     range: false,
+//     multipleDates: false,
+//     multipleDatesSeparator: " - ",
+//     offset: 5,
+//     position: "bottom right",
+//     classes: 'datepicker__date-type_masket',
+// });
