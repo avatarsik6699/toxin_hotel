3bba10372ffe7c29a8db01b2c745f4a5a81dff50
avatarsik <avatarsik6699@gmail.com>
2020-10-05 18:40:55 +0700

    added styles and html for the room block in cards section.

--- src/components/range-slider/range-slider.js
+++
@@ -1,9 +0,0 @@
-$('.target').slider({
-    range: true,
-    animate: 100,
-    classes: {
-        "ui-slider": "slider",
-        "ui-slider-handle": "slider__handle",
-        "ui-slider-range": "slider__range",
-    }
-});
