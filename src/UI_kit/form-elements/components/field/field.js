42c880b08876d9d07e89b0d6ba4c7a1302addd14
avatarsik <avatarsik6699@gmail.com>
2020-10-08 20:23:32 +0700

    Refactored the field. Added tabs in 2 spaces. Added a wrapper for the input so that the arrow does not move out if there is or is missing a label top.

--- src/UI_kit/form-elements/components/field/field.js
+++ src/UI_kit/form-elements/components/field/field.js
@@ -1,12 +1,16 @@
 import { Calendar } from 'root/UI_kit/cards/components/calendar/calendar';
-const field = document.querySelector('.text-field');
+const field = document.querySelectorAll('input.field__input[data-id]');
 
-if (field.dataset.type === 'masked') {
-    new Calendar('.text-field', {
-        range: false,
-        multipleDates: false,
-        multipleDatesSeparator: " - ",
-        offset: 5,
-        position: "bottom right",
-    });
-}
+Array.from(field).forEach ((el, index, arr) => {
+    if (arr.length !== 0) {
+        if (el.dataset.masked !== 'false') {
+            new Calendar(el, {
+                range: false,
+                multipleDates: false,
+                multipleDatesSeparator: " - ",
+                offset: 5,
+                position: "bottom right",
+            });
+        }
+    }
+})
