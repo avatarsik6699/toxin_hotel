aab93fe0fabb51dcef1053b719268ff8323a5e57
avatarsik <avatarsik6699@gmail.com>
2020-10-09 20:21:33 +0700

    Refactored the checkbox. Changed the html (added a fake span, to display a fake input and a check_mark.svg, when checked).

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import 'FEcomponents/field/field';
 import { Dropdown } from 'FEcomponents/dropdown/dropdown';
 import { Calendar } from 'root/UI_kit/cards/components/calendar/calendar';
 
-const drop21 = new Dropdown('div.dropdown[data-id="9"]', {})
-const dropdown = new Dropdown('div.dropdown[data-id="12"]', {
-  exceptions: ['взрослые', 'дети'],
-  exceptionDeclension: 'гости',   
-  declensions: {
-      'младенцы': ['младенец', 'младенца', 'младенцев'],
-      'гости': ['гость', 'гостя', 'гостей']
-  }
-})
-const drop = new Dropdown('div.dropdown-wrapper[data-id="55"]', {})
-const drop2 = new Dropdown('div.dropdown[data-id="10"]', {})
+// const drop21 = new Dropdown('div.dropdown[data-id="9"]', {})
+// const dropdown = new Dropdown('div.dropdown[data-id="12"]', {
+//   exceptions: ['взрослые', 'дети'],
+//   exceptionDeclension: 'гости',   
+//   declensions: {
+//       'младенцы': ['младенец', 'младенца', 'младенцев'],
+//       'гости': ['гость', 'гостя', 'гостей']
+//   }
+// })
+// const drop = new Dropdown('div.dropdown-wrapper[data-id="55"]', {})
+// const drop2 = new Dropdown('div.dropdown[data-id="10"]', {})
 
 // new Calendar('input[data-id="1"]', {
 //     range: false,
