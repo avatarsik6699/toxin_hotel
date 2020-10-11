cca8801177cfdaea3c95166a3495176a655785cf
avatarsik <avatarsik6699@gmail.com>
2020-10-11 18:31:46 +0700

    added js(dropdown, calendar) for the form-elements page

---
+++ src/UI_kit/form-elements/form-elements.js
@@ -0,0 +1,30 @@
+import { Dropdown } from 'FEcomponents/dropdown/dropdown';
+import { Calendar } from 'root/UI_kit/cards/components/calendar/calendar';
+import 'FEcomponents/range-slider/range-slider';
+
+const dropdown_default = new Dropdown('div.dropdown[data-id="1"]', {
+  exceptions: ['взрослые', 'дети'],
+  exceptionDeclension: 'гости',   
+  declensions: {
+      'младенцы': ['младенец', 'младенца', 'младенцев'],
+      'гости': ['гость', 'гостя', 'гостей']
+  }
+})
+const field_masked = new Calendar('input.field__input[data-id="2"]', {
+  range: false,
+  multipleDates: false,
+  multipleDatesSeparator: " - ",
+  offset: 5,
+  position: "bottom right",
+});
+const dropdown_date = new Dropdown('div.dropdown-wrapper[data-id="3"]', {})
+const dropdown_filter = new Dropdown('div.dropdown[data-id="4"]', {})
+const dropdown_default_comfort = new Dropdown('div.dropdown[data-id="5"]', {})
+const dropdown_default_guests = new Dropdown('div.dropdown[data-id="6"]', {
+  exceptions: ['взрослые', 'дети'],
+  exceptionDeclension: 'гости',   
+  declensions: {
+      'младенцы': ['младенец', 'младенца', 'младенцев'],
+      'гости': ['гость', 'гостя', 'гостей']
+  }
+})
--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
 import './style.scss';
-import 'FEcomponents/field/field';
+// import 'FEcomponents/field/field';
 import { Dropdown } from 'FEcomponents/dropdown/dropdown';
 import { Calendar } from 'root/UI_kit/cards/components/calendar/calendar';
 import 'FEcomponents/range-slider/range-slider';
-// const drop21 = new Dropdown('div.dropdown[data-id="9"]', {})
-// const dropdown = new Dropdown('div.dropdown[data-id="12"]', {
-//   exceptions: ['взрослые', 'дети'],
-//   exceptionDeclension: 'гости',   
-//   declensions: {
-//       'младенцы': ['младенец', 'младенца', 'младенцев'],
-//       'гости': ['гость', 'гостя', 'гостей']
-//   }
-// })
-// const drop = new Dropdown('div.dropdown-wrapper[data-id="55"]', {})
-// const drop2 = new Dropdown('div.dropdown[data-id="10"]', {})
 
+const dropdown_default = new Dropdown('div.dropdown[data-id="1"]', {
+  exceptions: ['взрослые', 'дети'],
+  exceptionDeclension: 'гости',   
+  declensions: {
+      'младенцы': ['младенец', 'младенца', 'младенцев'],
+      'гости': ['гость', 'гостя', 'гостей']
+  }
+})
+const field_masked = new Calendar('input.field__input[data-id="2"]', {
+  range: false,
+  multipleDates: false,
+  multipleDatesSeparator: " - ",
+  offset: 5,
+  position: "bottom right",
+});
+const dropdown_date = new Dropdown('div.dropdown-wrapper[data-id="3"]', {})
+const dropdown_filter = new Dropdown('div.dropdown[data-id="4"]', {})
+const dropdown_default_comfort = new Dropdown('div.dropdown[data-id="5"]', {})
+const dropdown_default_guests = new Dropdown('div.dropdown[data-id="6"]', {
+  exceptions: ['взрослые', 'дети'],
+  exceptionDeclension: 'гости',   
+  declensions: {
+      'младенцы': ['младенец', 'младенца', 'младенцев'],
+      'гости': ['гость', 'гостя', 'гостей']
+  }
+})
 // new Calendar('input[data-id="1"]', {
 //     range: false,
 //     multipleDates: false,
