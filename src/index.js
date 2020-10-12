58238f87ba7ba244a65a2857dcc136c639f217f9
avatarsik <avatarsik6699@gmail.com>
2020-10-12 15:16:31 +0700

    Refactored calendar and room block from cards

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import './style.scss';
 import { Calendar } from 'root/UI_kit/cards/components/calendar/calendar';
-import { Dropdown } from 'FEcomponents/dropdown/dropdown';
-// new Calendar('input[data-id="registration_1"]', {
-//   range: false,
-//   multipleDates: false,
-//   multipleDatesSeparator: " - ",
-//   offset: 5,
-//   position: "bottom right",
-//   classes: 'datepicker__date-type_masket',
+// import { Dropdown } from 'FEcomponents/dropdown/dropdown';
+
+// new Calendar('.calendar', {
+//   inline: true,
 // });
 
 // import 'FEcomponents/range-slider/range-slider';
